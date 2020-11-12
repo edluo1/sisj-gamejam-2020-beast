fc317556cd77b6760f2ac5d8d03af382346ab8f6
Edward Luo <edluo1@bu.edu>
2020-11-12 01:42:26 -0800

    Converted player, Benji, and UI to prefabs and added basic enemy with charging attack

---
+++ Assets/Scripts/Enemies/BasicEnemyBehavior.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum EnemyState 
+{
+	Search,
+	Charge,
+	Attack
+}
+
+public class BasicEnemyBehavior : MonoBehaviour
+{
+	public GameObject player; // who to target
+	public float inRange; // when player is in range
+	public float chargeTime;
+	float chargeTimeRemaining;
+	SpriteRenderer spriteRenderer;
+
+	EnemyState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = EnemyState.Search;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        chargeTimeRemaining = chargeTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if (state == EnemyState.Search) {
+    		Vector3 targetPosition = player.transform.position;
+	        Vector3 direction = (targetPosition - transform.position).normalized;
+
+	        transform.rotation = Quaternion.FromToRotation(Vector3.right, direction);
+
+	        transform.Translate(Vector3.right * Time.deltaTime);
+
+	        // Debug.Log(Vector3.Distance(targetPosition, transform.position));
+	        if (Vector3.Distance(targetPosition, transform.position) <= inRange) {
+	        	state = EnemyState.Charge;
+	        }
+    	}
+    	else if (state == EnemyState.Charge) {
+    		float chargeRatio = (chargeTimeRemaining / chargeTime);
+            Debug.Log(chargeRatio);
+    		spriteRenderer.color = new Color(1, chargeRatio, chargeRatio); // charge up attack by turning red
+    		chargeTimeRemaining -= Time.deltaTime;
+    		if (chargeTimeRemaining <= 0) {
+    			state = EnemyState.Attack;
+    		}
+    	}
+    	else if (state == EnemyState.Attack) {
+    		chargeTimeRemaining = chargeTime;
+
+    		spriteRenderer.color = new Color(1, 1, 1);
+    		state = EnemyState.Search;
+    	}
+
+    }
+}
---
+++ Assets/Scripts/Mechanics/HealthScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    public int maxHealth = 100;
+
+    public int currentHealth = 100;
+
+    public HealthBarScript healthBarScript; // Only for use by player. Ties health to health bar.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthBarScript != null)
+        {
+            healthBarScript.maxHealth = maxHealth;
+            healthBarScript.currentHealth = currentHealth;
+        }
+
+        if (currentHealth <= 0)
+        {
+            // TODO add code to play death animation
+
+            Destroy(gameObject);
+        }
+    }
+}
