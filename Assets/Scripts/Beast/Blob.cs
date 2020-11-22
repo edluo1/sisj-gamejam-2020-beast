e72785d79fd6ea35e878b359da1b0b88ba2885a2
Newbhope <newbhope8@gmail.com>
2020-11-21 22:08:57 -0800

    Add bouncing slime

---
+++ Assets/Scripts/Beast/Blob.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blob : MonoBehaviour
+{
+    public GameObject player;
+    public float movementSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = player.transform.position;
+        Vector3 direction = (targetPosition - transform.position).normalized;
+
+        transform.Translate(direction * Time.deltaTime * movementSpeed);
+    }
+}
