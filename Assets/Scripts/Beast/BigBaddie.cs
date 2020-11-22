aa0705ffcdad47a2f9dce8165836e17fe93aeaca
Newbhope <newbhope8@gmail.com>
2020-11-21 22:31:56 -0800

    Add big baddie brute

--- Assets/Scripts/Beast/BigBaddie.cs
+++ Assets/Scripts/Beast/BigBaddie.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Blob : MonoBehaviour
+public class BigBaddie : MonoBehaviour
 {
     public GameObject player;
     public float movementSpeed = 1f;
 
+    private SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -19,5 +22,10 @@ public class Blob : MonoBehaviour
         Vector3 direction = (targetPosition - transform.position).normalized;
 
         transform.Translate(direction * Time.deltaTime * movementSpeed);
+
+        if (transform.position.x > targetPosition.x)
+        {
+            spriteRenderer.flipX = true;
+        }
     }
 }
