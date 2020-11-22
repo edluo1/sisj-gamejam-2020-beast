0fbb02d3942fe1c1795417714092febd5a3cf049
Edward Luo <edluo1@bu.edu>
2020-11-22 10:30:16 -0800

    Set up scene transition

---
+++ Assets/Scripts/Mechanics/RoomExitScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomExitScript : MonoBehaviour
+{
+	public bool Enterable = false;
+	public string sceneName;
+
+	TilemapCollider2D collider;
+    Animator animator;
+    
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        collider = GetComponent<TilemapCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Enterable) {
+        	collider.isTrigger = true;
+        }
+    }
+
+    public void OpenRoomExit() {
+    	Enterable = true;
+    	animator.SetBool("Enterable", true);
+    }
+
+    public void OnTriggerEnter() {
+    	if (sceneName) {
+    		SceneManager.LoadScene(sceneName);
+    	}
+    	else {
+    		Debug.Log("No scene to load!");
+    	}
+    }
+}
