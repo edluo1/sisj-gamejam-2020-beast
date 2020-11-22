26f92ee297e6cb5707b9c5d6f89326675023afca
Edward Luo <edluo1@bu.edu>
2020-11-22 11:15:09 -0800

    Benji and player now load at spawnpoint

---
+++ Assets/Scripts/Mechanics/SpawnpointScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnpointScript : MonoBehaviour
+{
+	public GameObject toSpawn;
+
+	// spawnpoint for the character
+	// not for the enemy that's called the spawner
+    // Start is called before the first frame update
+    void Start()
+    {
+        string tag = toSpawn.tag;
+        GameObject[] allToSpawn = GameObject.FindGameObjectsWithTag(tag);
+        if (allToSpawn.Length <= 0) {
+        	Instantiate(toSpawn, transform.position, transform.rotation);
+        } else {
+        	allToSpawn[0].transform.position = transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
